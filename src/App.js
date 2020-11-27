f08fda2b1351bf380bf6f631e8eb8cbf53f92175
Boariu Diana <boariu.diana11@gmail.com>
2020-11-27 14:33:09 +0200

    localStorage add

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
-
+import React from 'react'
 import './App.css';
-import Header from './shared/Header';
-import SearchBox from "./search/SearchBox"
+import Header from './shared/Header'
+import SearchBox from './search/SearchBox'
+import SavedMovies from './SavedMovies/SavedMovies'
 
 
+class App extends React.Component {
 
-const App = ()=>{
-  return (
-    <div className="App">
-      <Header/>
-      <SearchBox/>
-    </div>
-  );
+  constructor(props) {
+    super(props)
+    const movies = JSON.parse(window.localStorage.getItem('saved-movies'))
+    if (movies && Array.isArray(movies)) {
+      this.state = {
+        movies,
+      }
+    } else {
+      this.state = {
+        movies: [],
+      }
+    }
+  }
+
+handleAddMovie = (movie)=>{
+  const movies = this.state.movies
+  this.setState(
+    {
+      movies: [...movies, movie],
+    },
+    () => {
+      window.localStorage.setItem(
+        'saved-movies',
+        JSON.stringify(this.state.movies),
+      )
+    },
+  )
 }
 
-export default App;
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <SearchBox  onMovieAdd={this.handleAddMovie}/>
+        <SavedMovies savedMovies={this.state.movies} />
+      </div>
+    )
+  }
+}
+
+export default App
--- src/search/SearchBox.js
+++ src/search/SearchBox.js
@@ -1,41 +1,73 @@
-import React, { useState } from 'react'
-import { searchMovies } from '../shared/API'
+import { TextField, Button } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import React, { useState } from 'react';
+import { searchMovies } from '../shared/API';
+import styles from './SearchBox.module.css';
 
 const MovieList = (props) => {
   return (
-    <div>
+    <div className={styles.listItem}>
       <ul>
         {props.movies.map((movie) => (
-          <li>
+          <li className={styles.listItem} key={movie.id}>
             <b>{movie.title}</b> ({movie.release_date})
+            <div>
+              <img src={`http://image.tmdb.org/t/p/w300/${movie.poster_path}`} alt={`${movie.title} logo`}/>
+              </div>
+            <Button
+              className="Add Movie"
+              color="secondary"
+              onClick={(e) => {
+                e.preventDefault();
+                props.onMovieAdd(movie);
+              }}
+            >
+              <AddIcon /> Add Movie
+            </Button>
           </li>
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-const SearchBox = () => {
-  const [term, setTerm] = useState('')
-  const [movies, setMovies] = useState([])
+const SearchBox = (props) => {
+  const [term, setTerm] = useState('');
+  const [movies, setMovies] = useState([]);
+
+  const localMovieAdd = (movie) => {
+    setMovies([]);
+    props.onMovieAdd(movie);
+  };
+
   return (
-    <div>
-      <input
-        placeholder="Search for a movie"
+    <div className = {styles.main}>
+      <TextField
+        variant="outlined"
+        label="Search for a movie"
         value={term}
         onChange={(e) => {
-          setTerm(e.target.value)
+          setTerm(e.target.value);
         }}
+        onKeyPress={(e) => {
+          if (e.key === 'Enter') {
+          searchMovies(term).then((res) => setMovies(res.data.results))
+          }
+        }
+      }
       />
-      <button
+      <Button
+        variant="contained"
+        color="primary"
         onClick={() =>
           searchMovies(term).then((res) => setMovies(res.data.results))
-        }>
+        }
+      >
         Search
-      </button>
-      <MovieList movies={movies} />
+      </Button>
+      <MovieList movies={movies} onMovieAdd={localMovieAdd} />
     </div>
-  )
-}
+  );
+};
 
-export default SearchBox
+export default SearchBox;
